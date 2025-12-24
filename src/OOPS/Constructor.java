7163917fe10a96dd9222d6325e2154a74c2c5241
pateladitya2580 <pateladitya2580@gmail.com>
2025-12-24 18:26:18 +0530

    .

--- src/OOPS/Constructor.java
+++ src/OOPS/Constructor.java
@@ -5,12 +5,17 @@ public class Constructor {
         String name;
         int rno;
         double percentage;
+        final String schoolName = "SNPS";// ye ek final variable hai isko koi change nahi kar sakta
+        //default constructor
+        public Student(){
+
+        }
         //constructor declaration
-        public Student(String naam,int roll ,double per)
+         public Student(String name,int rno ,double percentage)
         {
-            name = naam;
-            rno = roll;
-            percentage = per;
+            this.name = name;
+            this.rno = rno;
+            this.percentage = percentage;
         }
     }
     static void main(String[] args) {
@@ -18,5 +23,17 @@ public class Constructor {
         System.out.println(s1.name);
         System.out.println(s1.rno);
         System.out.println(s1.percentage);
+        System.out.println(s1.schoolName);
+
+        Student s2 = new Student();
+        s2.name = "Parag";
+        s2.rno = 1012;
+        s2.percentage = 99;
+        System.out.println(s2.name);
+        System.out.println(s2.rno);
+        System.out.println(s2.percentage);
+        System.out.println(s2.schoolName);
+
     }
 }
+// default constructor bhi bana do error se bachne ke liye
